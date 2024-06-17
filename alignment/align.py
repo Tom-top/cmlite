328546f7cc52486fa4aa18ddcd703710048cec65
Tom-top <thomas.topilko@gmail.com>
2024-06-17 14:27:07 +0200

    TTO: remove elastix log files in root

--- alignment/align.py
+++ alignment/align.py
@@ -8,6 +8,7 @@ import SimpleITK as sitk
 
 import utils.utils as ut
 
+
 def align_images(fixed_image_path, moving_image_path, parameters_path, output_dir, output_file_name,
                  transform_file_name):
     if not os.path.exists(output_dir):
@@ -24,10 +25,29 @@ def align_images(fixed_image_path, moving_image_path, parameters_path, output_di
 
     # Read parameter map from file
     parameter_map = sitk.ReadParameterFile(parameters_path)
+
+    # Set the output directory for elastix log files
+    log_output_dir = os.path.join(output_dir, 'elastix_logs')
+    if not os.path.exists(log_output_dir):
+        os.makedirs(log_output_dir)
+
+    # Set additional parameters in the parameter map
+    parameter_map['WriteLogFile'] = ['true']
+    parameter_map['WriteResultImageAfterEachResolution'] = ['false']
+    parameter_map['WriteResultImage'] = ['false']
+
     elastix_image_filter.SetParameterMap(parameter_map)
 
-    # Perform registration
-    elastix_image_filter.Execute()
+    # Temporarily change the working directory to the log output directory
+    original_cwd = os.getcwd()
+    os.chdir(log_output_dir)
+
+    try:
+        # Perform registration
+        elastix_image_filter.Execute()
+    finally:
+        # Revert to the original working directory
+        os.chdir(original_cwd)
 
     # Get the result image
     result_image = elastix_image_filter.GetResultImage()
