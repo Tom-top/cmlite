658e7740bd628a2f428fd680ab6976d66ba202e1
Tom-top <thomas.topilko@gmail.com>
2024-07-23 13:12:48 +0200

    TTO: testing pre-commit hook

--- spatial_transcriptomics/analysis/routine/binarize_reference.py
+++ spatial_transcriptomics/analysis/routine/binarize_reference.py
@@ -3,6 +3,7 @@ import os
 import tifffile
 
 # Binarize the entire annotation
+test_path = "hello"  # PERSONAL
 ref_directory = r"/mnt/data/spatial_transcriptomics/results/whole_brain"  # PERSONAL
 ref_path = os.path.join(ref_directory, f"atlas_annotations_coronal.tif")
 
